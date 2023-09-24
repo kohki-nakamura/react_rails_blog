961887bb44703ab236d34def7e6e70aad9ee3f9c
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2023-09-24 21:08:55 +0900

    ブログコントローラーとヘルスチェックを追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+   # ヘルスチェック
+   get '/health_check', to: 'health_checks#index'
+
+   resources :blogs, only: [:index, :show, :create]
 end
